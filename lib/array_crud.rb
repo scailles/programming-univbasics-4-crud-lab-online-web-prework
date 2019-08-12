d9b75b9b5dcfe4fad4bd7b5dd7a75d0943baa67c
celebrated-integer-2023 <scailles23@gmail.com>
2019-08-12 12:50:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   element=yellow
   create_an_array.push("yellow")
-  print create_an_array
+  return create_an_array
 end
 
 def add_element_to_start_of_array(array, element)
