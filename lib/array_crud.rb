ff24378fd87bc181dca7c843f5003ae0df15fd73
celebrated-integer-2023 <scailles23@gmail.com>
2019-08-12 12:59:29 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,33 +1,39 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["red","blue","green","brown"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  element=yellow
+  do
+  create_an_array.push("yellow")
+  return create_an_array
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  do
+  element="purple"
+  create_an_array.unshift("purple")
 end
 
 def remove_element_from_end_of_array(array)
+  do create_an_array.pop
   
 end
 
 def remove_element_from_start_of_array(array)
-  
+  do create_an_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  do create_an_array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  return create_an_array[0]
 end
 
 def retrieve_last_element_from_array(array)
